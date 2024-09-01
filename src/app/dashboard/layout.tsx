b28ae894c10b54f7dd757240b9ecf56f4fe8965d
lakshya-roonwal <lakshyaroonwal@gmail.com>
2024-09-01 22:34:23 +0530

    chore : preinstall & dashboard label

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -35,7 +35,7 @@ const Logo = () => {
         animate={{ opacity: 1 }}
         className="font-medium text-black dark:text-white whitespace-pre"
       >
-        Acet Labs
+        Micro Fund
       </motion.span>
     </Link>
   );
